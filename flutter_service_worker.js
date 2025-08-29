0006f7ba4d1d45fd677929ff487880a29a8177a2
DESKTOP-QUKUMQBA0_Win_NoteBook <ssaltteokchap7@gmail.com>
2025-08-29 10:58:04 +0900

    deploy flutter web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -61,7 +61,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "681d507c0f066d90a668e9a6c0ac8190
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "67ca1d39056f4352254425c167f8358e",
+"flutter_bootstrap.js": "be2157211a5c553ce0664e821fc25198",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
